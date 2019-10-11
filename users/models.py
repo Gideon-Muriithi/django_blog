74c8a0fe0aef5bba3940135bbaeae199339edd34
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-11 11:14:29 +0300

    Add changes of the files

--- users/models.py
+++ users/models.py
@@ -10,10 +10,10 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
     def save(self):
-        super.save()
+        super().save()
         img = Image.open(self.image.path)
 
-        if image.height > 300 or width > 300:
+        if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
